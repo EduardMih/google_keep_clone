07b54d141754ab18b16d530f717523398c619763
EduardMih <hamza.eduard@yahoo.com>
2022-05-13 13:35:39 +0300

    refactored

--- lib/screens/all_notes.dart
+++ lib/screens/all_notes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_keep_clone/screens/edit_note.dart';
 
+const iconColor = Color(0xFF5F6368);
 
 class AllNotesView extends StatefulWidget {
 
@@ -20,41 +21,63 @@ class _AllNotesViewState extends State<AllNotesView> {
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text("App bar")),
+    return Scaffold(
+        appBar: AppBar(title: const Text("App bar"), ),
         body: getBody(),
-        floatingActionButton: FloatingActionButton(
+        floatingActionButton: _getFloatingActionButton(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+        bottomNavigationBar: _getBottomAppBar(),
+        );
+  }
+
+  FloatingActionButton _getFloatingActionButton()
+  {
+
+    return FloatingActionButton(
           child: const Icon(Icons.note_add),
           onPressed: () {
             Navigator.push(
               context, 
               MaterialPageRoute(builder: (context) => EditNoteView()));
           },
-          backgroundColor: Colors.blue,
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.redAccent,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+    );
+
+  }
+
+  BottomAppBar _getBottomAppBar()
+  {
+
+    return BottomAppBar(
+          color: Theme.of(context).colorScheme.onSecondary,
           shape: const CircularNotchedRectangle(),
           notchMargin: 5,
           child: Row(
             mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
             IconButton(
+              onPressed: () => {}, 
+              icon: const Icon(Icons.check_box, color: iconColor)
+              ),
+            IconButton(
+              onPressed: () => {}, 
+              icon: const Icon(Icons.brush_sharp, color: iconColor)
+              ),
+            IconButton(
               onPressed: () => {},
-              icon: const Icon(Icons.menu, color: Colors.white)),
+              icon: const Icon(Icons.mic, color: iconColor)
+              ),
             Padding(
               padding: const EdgeInsets.only(right: 90),
               child: IconButton(
                 onPressed: () => {}, 
-                icon: const Icon(Icons.abc, color: Colors.white))
+                icon: const Icon(Icons.insert_photo, color: iconColor))
               )
           ]),
-        ),
-        ),
         );
 
   }
+
+
 }
